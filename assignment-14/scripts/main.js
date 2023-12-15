349e75778e05aa3cee74c7f7cc8a65464249b65d
Jennifer Hale <jenniferhale42@outlook.com>
2023-12-15 14:14:10 -0700

    finished javascript and form

--- assignment-14/scripts/main.js
+++ assignment-14/scripts/main.js
@@ -1,5 +1,9 @@
-var c = document.getElementById("myCanvas");
+var c = document.getElementById("fake-logo");
 var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
+ctx.font = "30px Arial";
+ctx.strokeText("Round Box", 150, 50);
+ctx.font = "30px Arial";
+ctx.strokeText("Publishing", 150, 80);
